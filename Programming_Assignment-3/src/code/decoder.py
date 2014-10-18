7235ec496a3760065f0202034803b471e32066d2
Vadim <solovievadim@gmail.com>
2014-10-18 15:43:41 +1100

    Textfile loader

--- Programming_Assignment-3/src/code/decoder.py
+++ Programming_Assignment-3/src/code/decoder.py
@@ -1,7 +1,23 @@
 import numpy as np
 import os.path
-import sys
+import copy
 
 basepath = os.path.dirname(__file__)
-train_filepath = os.path.abspath(os.path.join(basepath, "..", "data", "train.txt"))
-train_struct_filepath = os.path.abspath(os.path.join(basepath, "..", "data", "train_struct.txt"))
+decode_input_filepath = os.path.abspath(os.path.join(basepath, "..", "data", "decode_input.txt"))
+#train_filepath = os.path.abspath(os.path.join(basepath, "..", "data", "train.txt"))
+#train_struct_filepath = os.path.abspath(os.path.join(basepath, "..", "data", "train_struct.txt"))
+
+
+decode_input = np.loadtxt(open(decode_input_filepath,"rb"))
+
+
+x_dash = np.array(decode_input[:12800]).reshape((100, 128))
+w_dash = np.array(decode_input[12800:16128]).reshape((26, 128))
+T_dash = np.array(decode_input[16128:]).reshape((26, 26))
+
+# decode_ input
+# 128 for each x, 100 tota
+# Each of the 26 letters is represented by 1x128 weights
+#[128] x 100
+#[128] x 26
+# 26 x 26 transition
