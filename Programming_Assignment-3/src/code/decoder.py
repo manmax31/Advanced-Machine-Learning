6135fa2e8dd9216fe04cda4fd620cb4370e90308
vadim <solovievadim@gmail.com>
2014-10-18 14:38:59 +1100

    Basic  loader

---
+++ Programming_Assignment-3/src/code/decoder.py
@@ -0,0 +1,7 @@
+import numpy as np
+import os.path
+import sys
+
+basepath = os.path.dirname(__file__)
+train_filepath = os.path.abspath(os.path.join(basepath, "..", "data", "train.txt"))
+train_struct_filepath = os.path.abspath(os.path.join(basepath, "..", "data", "train_struct.txt"))
