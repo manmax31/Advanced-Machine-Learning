45106801335e6b550822dc85d5aea859310fcfbf
manmax31 <manabchetia@Manabs-MacBook-Air.local>
2014-10-20 16:19:44 +1100

    reverse list fixed

--- Programming_Assignment-3/src/code/CRF/Viterbi.py
+++ Programming_Assignment-3/src/code/CRF/Viterbi.py
@@ -1,94 +1,98 @@
-'''
-Q 1c. MaxSum
-This script implements maxsum algorithm to decode the file decode_input.txt
-'''
+__author__ = 'manab chetia'
+"""
+Q 1c. Max-Sum
+This script implements max-sum algorithm to decode the file decode_input.txt
+"""
 
 import numpy as np
 
-'''
-This function formats the data into X, W, T from the given file
-'''
-def formatData(raw_data):
+
+def format_data(raw_data):
+    """ This function formats the data into X, W, T from the given file 
+    :rtype : numpy arrays
+    """
     m         = 100
     n_letters = 26
     vec_len   = 128
 
-    X = np.array    (           raw_data[:(m*vec_len)]).reshape((m, vec_len)                                              )
-    W = np.array    (           raw_data[(m*vec_len):(m*vec_len+n_letters*vec_len)]).reshape((n_letters, vec_len)         )
-    T = np.transpose( np.array( raw_data[(m*vec_len+n_letters*vec_len):]).reshape((n_letters, n_letters)  )               )
+    X = np.array(raw_data[:(m * vec_len)]).reshape((m, vec_len))
+    W = np.array(raw_data[(m * vec_len):(m * vec_len + n_letters * vec_len)]).reshape((n_letters, vec_len))
+    T = np.transpose(np.array(raw_data[(m * vec_len + n_letters * vec_len):]).reshape((n_letters, n_letters)))
 
     return X, W, T
 
-'''
-This function gets the most likely transition state for a state from all possible states in the previous layer
-'''
-def getMaxStates(X, W, T):
-    maxStates  = {}
+
+def get_max_states(X, W, T):
+    """ This function gets the most likely transition state for a state from all possible states in the previous layer 
+    :rtype : dictionary
+    """
+    max_states = {}
     for x_row, li in zip(X, xrange(1, X.shape[0])):
-        maxStates[li] = []
+        max_states[li] = []
         for t_row in T:
             values = []
             for w_row, t_ij in zip(W, t_row):
-                values.append( np.dot(x_row, w_row) + t_ij )
+                values.append(np.dot(x_row, w_row) + t_ij)
 
-            maxIndex = np.argmax(values)
-            maxStates[li].append(maxIndex)
-    return maxStates
+            max_index = np.argmax(values)
+            max_states[li].append(max_index)
+    return max_states
 
-'''
-This function gets all the 26 possible paths
-'''
-def getPaths(maxStates, W, X):
+
+def get_paths(max_states, W, X):
+    """ This function gets all the 26 possible paths """
     paths = {}
-    for e,i in zip(maxStates[len(maxStates)], xrange(W.shape[0])):
-        paths[i]=[e]
+    for e, i in zip(max_states[len(max_states)], xrange(W.shape[0])):
+        paths[i] = [e]
 
     for pi in paths:
         path = paths[pi]
-        for index in xrange(X.shape[0]-1, 0, -1):
-            path.append(maxStates[index][path[-1]])
+        for index in xrange(X.shape[0] - 1, 0, -1):
+            path.append(max_states[index][path[-1]])
 
     return paths
 
-'''
-This function gets the most likely state based on all 26 possible states
-'''
-def getMostLikelyPath(paths, T):
+
+def get_most_likely_path(paths, T):
+    """ This function gets the most likely state based on all 26 possible states
+    :rtype : list
+    """
     Tp = np.transpose(T)
 
     scores = []
     for pi in paths:
         path = paths[pi]
         score = 0.0
-        for i in xrange(len(path)-1):
-            score += Tp[path[i]][path[i+1]]
+        for i in xrange(len(path) - 1):
+            score += Tp[path[i]][path[i + 1]]
         scores.append(score)
 
-    maxPathIndex =  np.argmax(scores)
-    mostLikelyPath = paths[maxPathIndex]
-    return mostLikelyPath
+    # print("Score of max path:{}".format(max(scores)))
+    max_path_index = np.argmax(scores)
+    most_likely_path = paths[max_path_index]
+    return most_likely_path
 
-'''
-This function write the states of the most likely path to the file
-'''
-def writeToFile(mostLikelyPath, filepath):
-    f = open(filepath, 'w')
-    for state in mostLikelyPath:
-        f.write(str(state) + "\n")
+
+def write_to_file(most_likely_path, file_path):
+    """ This function write the states of the most likely path to the file """
+    most_likely_path.reverse()
+    f = open(file_path, 'w')
+    for state in most_likely_path:
+        f.write(str(state + 1) + "\n")
     f.close()
     print "Successfully created decode_output.txt "
 
-'''MAIN method'''
+
 def main():
+    """ MAIN method : Execution starts here """
     path = "../../data/"
     data = np.loadtxt(path + "decode_input.txt")
 
-    X, W, T = formatData(data)
+    X, W, T          = format_data(data)
+    max_states       = get_max_states(X, W, T)
+    paths            = get_paths(max_states, W, X)
+    most_likely_path = get_most_likely_path(paths, T)
+    write_to_file(most_likely_path, "../../results/decode_output.txt")
 
-    maxStates      =  getMaxStates(X, W, T)
-    paths          =  getPaths(maxStates, W, X)
-    mostLikelyPath =  getMostLikelyPath(paths, T)
-    writeToFile(mostLikelyPath, "../../results/decode_output.txt")
 
-
-if __name__ == "__main__":main()
+if __name__ == "__main__": main()
