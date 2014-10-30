943323487b5cb518313b7252f4fc66192a2fe1f3
Libo Yin <andyinlibo@gmail.com>
2014-10-30 22:23:03 +1100

    added gradient code and result

---
+++ Programming_Assignment-3/src/code/CRF/gradient.py
@@ -0,0 +1,95 @@
+from math import exp, log
+import numpy as np
+
+
+def load_parameters():  # refers to: p
+    raw = np.loadtxt("data/model.txt")
+    w = np.array(raw[:26 * p]).reshape((26, p))
+    t = np.transpose(np.array(raw[26 * p:]).reshape((26, 26)))
+    return w, t
+
+
+def load_data():
+    with open("data/train.txt") as f:
+        lines = [line.split() for line in f]
+    data, x, y = [], [], []
+    for l in lines:
+        x.append(list(map(int, l[5:])))
+        y.append(ord(l[1]) - 97)
+        if l[2] == "-1":
+            data.append((np.array(x, dtype=np.bool), np.array(y, dtype=np.int)))
+            x, y = [], []
+    return data
+
+
+def forward_dp(w, t, x):
+    # for all possible paths from the first letter forwards, calculate the sum of partial, unormalized probabilities
+    m = len(x)
+    dp = np.zeros((m, 26), dtype=np.float64)
+    for i in range(0, 26):  # the first line of the dp table
+        dp[0, i] = exp(np.dot(w[i], x[0]))
+    for i in range(1, m):  # for all following lines of the dp table
+        for j in range(0, 26):  # for each possible previous letter
+            dp[i, j] = sum(dp[i-1, k] * exp(t[k, j]) for k in range(0, 26)) * exp(np.dot(w[j], x[i]))
+            # here we rewrite the exp(sum) as prod(exp)
+    return dp
+
+
+def backward_dp(w, t, x):
+    # for all possible paths from the last letter backwards, calculate the sum of partial, unormalized probabilities
+    m = len(x)
+    dp = np.zeros((m, 26), dtype=np.float64)
+    for i in range(0, 26):  # the last line of the dp table
+        dp[-1, i] = exp(np.dot(w[i], x[-1]))
+    for i in range(m-2, -1, -1):  # for all previous lines of the dp table
+        for j in range(0, 26):  # for each possible next letter
+            dp[i, j] = sum(dp[i+1, k] * exp(t[j, k]) for k in range(0, 26)) * exp(np.dot(w[j], x[i]))
+            # here we rewrite the exp(sum) as prod(exp)
+    return dp
+
+
+def gradient(w, t, x, y):
+    m = len(x)
+    forward = forward_dp(w, t, x)
+    backward = backward_dp(w, t, x)
+    z = sum(forward[-1])  # partition function
+    nabla_w = np.empty((26, p), dtype=np.float64)
+    for i in range(0, 26):  # for each letter to take gradient against
+        prob = x[0] * backward[0, i] + x[-1] * forward[-1, i]
+        # sum of partial, unnormalized probability of all paths who contain letter @i on any bit
+        for j in range(1, m-1):
+            prob += x[j] * (forward[j, i] * backward[j, i] / exp(np.dot(w[i], x[j])))
+        nabla_w[i] = sum(x[j] for j in range(0, m) if y[j] == i) - prob / z
+    nabla_t = np.zeros((26, 26), dtype=np.float64)
+    for i in range(1, m):  # for each transition in all possible paths
+        for j in range(0, 26):
+            for k in range(0, 26):
+                nabla_t[j, k] -= forward[i-1, j] * backward[i, k] * exp(t[j, k])
+                # sum of partial, unnormalized probability of all paths who contain transition <@j, @k> on any bit
+    nabla_t /= z
+    for i in range(1, m):
+        nabla_t[y[i-1], y[i]] += 1
+    return nabla_w, nabla_t
+
+
+def log_probability(w, t, x, y):
+    m = len(x)
+    prob = sum(np.dot(w[y[i]], x[i]) for i in range(0, m))
+    prob += sum(t[y[i], y[i+1]] for i in range(0, m-1))
+    z = sum(forward_dp(w, t, x)[-1])
+    return log(exp(prob) / z)
+
+p = 128
+w, t = load_parameters()
+log_prob, nablas_w, nablas_t = [], [], []
+for x, y in load_data():
+    log_prob.append(log_probability(w, t, x, y))
+    nw, nt = gradient(w, t, x, y)
+    nablas_w.append(nw)  # 26 * @p
+    nablas_t.append(nt)  # 26 * 26
+print(log_prob)
+with open("result/gradient.txt", mode="w") as f:
+    for i in np.reshape(sum(nablas_w) / len(nablas_w), 26 * p):
+        f.write(str(i) + "\n")
+    for i in np.reshape(sum(nablas_t) / len(nablas_t), 26 * 26):
+        f.write(str(i) + "\n")
